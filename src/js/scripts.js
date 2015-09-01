04bbbfaadc319308259d39cdcd8351ab2650d9b9
Stephen Young <steveyoung359@gmail.com>
2015-08-31 21:45:50 -0500

    made responsive

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,13 +7,13 @@ $.fn.colorize = function() {
 };
 
 $(function() {
-  $('.logo').hide();
-  $('.logo-container').hide();
+  $('.logo').grayscale();
 
   $('#fancySwitch').on('change', function(e) {
     if ($(e.target)[0].checked) {
-      $('.logo-container').show('fast');
-      $('.logo').show('fast');
+      $('.logo').colorize();
+      $('.main-nav').css('box-shadow', '0px 0px 5px #747474');
+      $('.main-nav').css('border-bottom', '0');
       $('a').css('color', '#43a0fa');
       $('.standard-section').css('border-left', 'solid 10px #8CE196');
       $('.standard-section').css('background-color', 'rgba(97, 139, 102, 0.09)');
@@ -21,14 +21,15 @@ $(function() {
       $('footer').css('background-color', 'rgba(97, 139, 102, 0.09)');
       $('article').css('box-shadow', '2px 2px 5px rgba(0, 0, 0, 0.14)');
     } else {
-      $('.logo-container').hide();
-      $('.logo').hide();
+      $('.logo').grayscale();
       $('a').css('color', '#000');
       $('.standard-section').css('border-left', '0');
       $('.standard-section').css('background-color', '#fff');
       $('footer').css('border-left', '0');
       $('footer').css('background-color', '#fff');
       $('article').css('box-shadow', 'none');
+      $('.main-nav').css('box-shadow', 'none');
+      $('.main-nav').css('border-bottom', '1px solid #BEB5B5');
     }
   });
 });
